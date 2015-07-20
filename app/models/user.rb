23aa13c5a39fe1fe2f5b140a14f0b8421e57ba60
ben whitley <ben.whitley@hotmail.co.uk>
2015-07-20 21:26:00 +0100

    Applied simple form

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-end
+#  attr_accessible :email, :password, :password_confirmation, :remember_me,
+#  		:first_name, :last_name, :profile_name
+
+ end
